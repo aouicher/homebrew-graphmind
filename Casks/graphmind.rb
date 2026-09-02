51caf70573f812c72493d0e3084023aa2b52ff06
Alexandre O. <aouicher@users.noreply.github.com>
2026-09-02 14:17:39 +0000

    Update GraphMind cask to v0.2.215

--- Casks/graphmind.rb
+++ Casks/graphmind.rb
@@ -1,14 +1,14 @@
 cask "graphmind" do
-  version "0.2.214"
+  version "0.2.215"
 
   on_arm do
-    url "https://github.com/aouicher/graphmind/releases/download/v0.2.214/GraphMind-macos-arm64.dmg"
-    sha256 "532c3b1fa0ac470de23bfab7a8eb8d366c5d0264ba514d850e5ffc2630c2d04a"
+    url "https://github.com/aouicher/graphmind/releases/download/v0.2.215/GraphMind-macos-arm64.dmg"
+    sha256 "203868502f05271744bff5b5b7b410fffc376eb9e38e3d7ce87d3576a6a3558e"
   end
 
   on_intel do
-    url "https://github.com/aouicher/graphmind/releases/download/v0.2.214/GraphMind-macos-x64.dmg"
-    sha256 "ed2703abf051d16c34e5680678f8a827c63872db594a3fdbf2d29394f19e8b55"
+    url "https://github.com/aouicher/graphmind/releases/download/v0.2.215/GraphMind-macos-x64.dmg"
+    sha256 "95d9e186b57ec4ea109399338e698deaf21f8a1edd9bf142122f593f0381c55a"
   end
 
   name "GraphMind"
